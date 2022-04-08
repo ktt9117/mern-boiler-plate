a29a0513bad9130cb7cc5715ca1609d4899b8ff3
김성중(기술지원 AA/Mobile) - 기술지원팀 <kim.sj@shinsegae.com>
2022-04-08 14:35:52 +0900

    Add module dotenv

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
-const app = express()
-const SERVER_PORT = 5100
-
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://mernboilerplate:mernboilerplate1234@movieappcluster.hymu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Database 
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch (err => console.log(err))
 
+// Route
 app.get('/', (req, res) => res.send("Hello express!"))
 
-app.listen(SERVER_PORT, () => console.log(`Server Listening on port ${SERVER_PORT}`))
+app.post('/register', (req, res) => {
+  //데이터베이스에 넣어줌
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err});
+    return res.status(200).json({ success: true });
+  })
+})
+
+app.listen(process.env.SERVER_PORT, () => console.log(`Server Listening on port ${process.env.SERVER_PORT}`))
