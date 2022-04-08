c584650ead8582b11fe98165f9916fce7c89c934
김성중(기술지원 AA/Mobile) - 기술지원팀 <kim.sj@shinsegae.com>
2022-04-08 15:19:21 +0900

    Fix: remove deprecated option(mongoDB 6.0)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // Database 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+  useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch (err => console.log(err))
 
