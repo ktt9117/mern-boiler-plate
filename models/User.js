33fee787d3b4b235a5d4ef9194d30d1e63eeb43f
김성중(기술지원 AA/Mobile) - 기술지원팀 <kim.sj@shinsegae.com>
2022-04-18 13:41:22 +0900

    Feature: encrypt the user password with bcrypt when register

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: { 
@@ -30,6 +32,21 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 });
+
+userSchema.pre('save', function( next ) {
+  let user = this;
+  if (user.isModified('password')) {
+    // 저장하기 전에 password 암호화
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err)
+        user.password = hash;
+        next()
+      })
+    })
+  }
+})
 
 const User = mongoose.model('User', userSchema);
 
